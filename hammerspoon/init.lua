55729795f7a34b15e54b1a7bd14ae39f248e6bc7
Ogulcan Girginc <ogirginc@ogirginc.com>
2021-02-19 15:26:50 +0300

    Add WindowGrid spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,6 +2,8 @@
 -- CONFIG
 -- Main configuration options.
 --------------------------------------------------------------------------------
+local hyper = {"ctrl", "alt"}
+
 --------------------------------------------------------------------------------
 -- SpoonInstall
 --------------------------------------------------------------------------------
@@ -20,6 +22,23 @@ hs.notify.new({title="Hammerspoon", informativeText="Config file is reloaded"}):
 --------------------------------------------------------------------------------
 -- SPOONS
 --------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- WindowGrid
+-- http://www.hammerspoon.org/docs/hs.grid.html#setMargins
+-- http://www.hammerspoon.org/docs/hs.geometry.html#size
+----------------------------------------
+--------------------------------------------------------------------------------
+SpoonInstall:andUse("WindowGrid", {
+                               config = { gridGeometries = { { "6x4" } } },
+     													 hotkeys = { show_grid = { hyper, "w" } },
+     													 start = true,
+     													--  hs.grid.ui.textSize = {10} -- Do not work
+     													--  hs.grid.setMargins(hs.geometry.size(1000, 1000)) -- TODO
+  														}
+)
+
+
 
 --------------------------------------------------------------------------------
 -- A ShiftIt like Hammerspoon window management configuration.
