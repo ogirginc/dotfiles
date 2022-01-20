a7969ef71900dcce8026bb54bddcf53a62231bc2
Oğulcan Girginç <ogirginc@ogirginc.com>
2022-01-20 20:15:39 +0300

    add trouble

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -93,6 +93,18 @@ return packer.startup(function(use)
     config = function() require'nvim-tree'.setup {} end
   }
 
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
   use "dyng/ctrlsf.vim"
   use { "junegunn/fzf", run = "./install --bin" }
   use "junegunn/fzf.vim"
