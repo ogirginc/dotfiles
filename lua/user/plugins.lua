e6e6a45243a112da68ba7f319b6d670d85c54e7e
Oğulcan Girginç <ogirginc@ogirginc.com>
2022-01-20 15:21:26 +0300

    add autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,8 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+  use "windwp/nvim-autopairs"
+
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
