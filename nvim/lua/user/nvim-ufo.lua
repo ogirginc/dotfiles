c5b28f62f81e5261efef7391a5834fb9c70d6e20
Oğulcan Girginç <ogirginc@ogirginc.com>
2022-09-06 22:27:09 +0300

    Fix fold not working for ts

--- nvim/lua/user/nvim-ufo.lua
+++ nvim/lua/user/nvim-ufo.lua
@@ -15,7 +15,7 @@ capabilities.textDocument.foldingRange = {
   dynamicRegistration = false,
   lineFoldingOnly = true
 }
-local language_servers = {} -- like {'gopls', 'clangd'}
+local language_servers = {'tsserver'} -- like {'gopls', 'clangd'}
 for _, ls in ipairs(language_servers) do
   require('lspconfig')[ls].setup({
     capabilities = capabilities,
