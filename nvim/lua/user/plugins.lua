1072d6f236ac1e78da54487657fc3cfae74ce434
Oğulcan Girginç <ogulcan@girginc.com>
2023-03-13 15:05:01 +0300

    Switch to 5 for plugin updates

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -31,7 +31,7 @@ end
 
 -- Have packer use a popup window
 packer.init {
-  -- max_jobs = 5, -- If packer sync fails
+  max_jobs = 5, -- If packer sync fails
   display = {
     open_fn = function()
       return require("packer.util").float { border = "rounded" }
