9705f125092750198c57a00b6c1308ea66b9c0f5
Oğulcan Girginç <ogulcan@girginc.com>
2023-03-15 16:31:08 +0300

    Switch to lsp-zero

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -55,27 +55,29 @@ return packer.startup(function(use)
   ------------------------------------------------------------------------------
   -- MODERN EDITOR FUNCTIONALITY
   ------------------------------------------------------------------------------
-    -- AUTOCOMPLETION
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-buffer"
-    use "hrsh7th/cmp-path"
-    use "hrsh7th/cmp-cmdline"
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {"neovim/nvim-lspconfig"},             -- Required
+      {"williamboman/mason.nvim"},           -- Optional
+      {"williamboman/mason-lspconfig.nvim"}, -- Optional
 
-    -- SNIPPETS
-    use "L3MON4D3/LuaSnip"
-    use "saadparwaiz1/cmp_luasnip"
-    use "rafamadriz/friendly-snippets"
+      -- Autocompletion
+      {"hrsh7th/nvim-cmp"},         -- Required
+      {"hrsh7th/cmp-nvim-lsp"},     -- Required
+      {"hrsh7th/cmp-buffer"},       -- Optional
+      {"hrsh7th/cmp-path"},         -- Optional
+      { "hrsh7th/cmp-cmdline" },      -- Optional
+      {"hrsh7th/cmp-nvim-lua"},     -- Optional
 
-    --- MANAGE & INSTALL LSP SERVERS, LINTERS & FORMATTERS
-    use "williamboman/mason.nvim"
-    use "williamboman/mason-lspconfig.nvim"
-
-    --- CONFIGURE LSP
-    use "neovim/nvim-lspconfig"
-    use "hrsh7th/cmp-nvim-lsp"
-    use { "glepnir/lspsaga.nvim", branch = "main" }
-    use "jose-elias-alvarez/typescript.nvim" -- additional functionality for ts server
-    use "onsails/lspkind.nvim" -- VSCode like icons for autocompletion
+      -- Snippets
+      {"L3MON4D3/LuaSnip"},             -- Required
+      {"saadparwaiz1/cmp_luasnip"},     -- Optional
+      {"rafamadriz/friendly-snippets"}, -- Optional
+    }
+  }
 
     -- FORMATTING & LINTING
     use "jose-elias-alvarez/null-ls.nvim"
