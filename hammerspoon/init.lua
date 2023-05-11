89ccf787cf53ecf2f3fa970b7923d384e2de6389
Oğulcan Girginç <ogulcan@girginc.com>
2023-05-11 11:31:35 +0300

    Remove zoom.spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -52,35 +52,3 @@ SpoonInstall.repos.ShiftIt = {
 
 SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
 spoon.ShiftIt:bindHotkeys({})
-
---------------------------------------------------------------------------------
--- This lets you click on the menu bar item to toggle the mute state.
--- https://github.com/jpf/Zoom.spoon
---------------------------------------------------------------------------------
-zoomStatusMenuBarItem = hs.menubar.new(nil)
-zoomStatusMenuBarItem:setClickCallback(function()
-    spoon.Zoom:toggleMute()
-end)
-
-updateZoomStatus = function(event)
-  hs.printf("updateZoomStatus(%s)", event)
-  if (event == "from-running-to-meeting") then
-    zoomStatusMenuBarItem:returnToMenuBar()
-  elseif (event == "muted") then
-    zoomStatusMenuBarItem:setTitle("🔴")
-  elseif (event == "unmuted") then
-    zoomStatusMenuBarItem:setTitle("🟢")
-  elseif (event == "from-meeting-to-running") or (event == "from-running-to-closed") then
-    zoomStatusMenuBarItem:removeFromMenuBar()
-  end
-end
-
-hs.loadSpoon("Zoom")
-spoon.Zoom:setStatusCallback(updateZoomStatus)
-spoon.Zoom:start()
-
--- Next up:
--- https://github.com/adamyonk/PushToTalk.spoon/blob/master/init.lua
-hs.hotkey.bind('', 'f12', function()
-  spoon.Zoom:toggleMute()
-end)
