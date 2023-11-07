7fd71045320805a6362a262c0e80eb0034edc7a6
Oğulcan Girginç <ogulcan@girginc.com>
2023-11-07 15:36:09 +0300

    Add tab back keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- You don't need this as you can use `<Leader><Tab>[` or `<Leader><Tab>]` instead!
+vim.keymap.set("n", "gb", "gT", { desc = "Go back one tab", noremap = true, silent = true })
