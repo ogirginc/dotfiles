c9c51d391d0113d2877d0407c0bbdc28f88202bc
Oğulcan Girginç <ogulcan@girginc.com>
2023-11-07 17:45:05 +0300

    Disable nvim-treesitter-context

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
