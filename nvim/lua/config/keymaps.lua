306b8b500ea9da03164eea3aa191e0f0a5111031
Oğulcan Girginç <ogulcan@girginc.com>
2023-11-10 16:50:12 +0300

    Add vim-test plugin

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,12 @@
 
 -- You don't need this as you can use `<Leader><Tab>[` or `<Leader><Tab>]` instead!
 vim.keymap.set("n", "gb", "gT", { desc = "Go back one tab", noremap = true, silent = true })
+
+---------------------------------------------------------------------------------
+-- PLUGINGS
+---------------------------------------------------------------------------------
+
+-- VIM TEST
+vim.keymap.set("n", "<leader>ttn", ":TestNearest<CR>", { desc = "Test nearest", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ttf", ":TestFile<CR>", { desc = "Test file", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ttl", ":TestLast<CR>", { desc = "Test last", noremap = true, silent = true })
