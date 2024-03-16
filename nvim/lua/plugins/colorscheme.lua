ad0303f88f00a9f732a1161cf3c9d100afa9f7e2
Oğulcan Girginç <ogulcan@girginc.com>
2024-03-16 10:30:38 +0300

    Use craftzdog's solarized colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,17 @@
 return {
-  -- Add colorscheme plugin
-  { "ishan9299/nvim-solarized-lua" },
+  -- -- Add colorscheme plugin
+  -- { "ishan9299/nvim-solarized-lua" },
+  { "craftzdog/solarized-osaka.nvim" },
 
   -- Configure LazyVim to load preferred colorscheme
   {
+    -- "ishan9299/nvim-solarized-lua",
     "LazyVim/LazyVim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
-      colorscheme = "solarized",
+      -- colorscheme = "solarized",
+      colorscheme = "solarized-osaka",
     },
-  }
+  },
 }
