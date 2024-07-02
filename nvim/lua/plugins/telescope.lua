0342db2162829835dd8efc7a70b96f92c0620135
Oğulcan Girginç <ogulcan@girginc.com>
2024-07-02 13:08:13 +0300

    Remove comment

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,7 +13,6 @@ return {
       override_generic_sorter = true, -- override the generic sorter
       override_file_sorter = true, -- override the file sorter
       case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-      -- the default case_mode is "smart_case"
     },
   },
   opts = function()
