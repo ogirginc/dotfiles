e603bd7b66da043eb3a180ec532bc78f337dcf92
Oğulcan Girginç <ogulcan@girginc.com>
2024-09-04 16:56:14 +0300

    Remove gT for tab

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- You don't need this as you can use `<Leader><Tab>[` or `<Leader><Tab>]` instead!
-vim.keymap.set("n", "gb", "gT", { desc = "Go back one tab", noremap = true, silent = true })
-
+-- TABS
 vim.keymap.set("n", "<leader><tab>-", ":tabm-<CR>", { desc = "Move tab to the left", noremap = true, silent = true })
 vim.keymap.set("n", "<leader><tab>+", ":tabm+<CR>", { desc = "Move tab to the right", noremap = true, silent = true })
 
