bec0620ed1020779df093c9bb8f1749959cefadf
Oğulcan Girginç <ogulcan@girginc.com>
2024-11-15 14:44:37 +0300

    Update nvim-window-picker

--- nvim/lua/plugins/nvim-window-picker.lua
+++ nvim/lua/plugins/nvim-window-picker.lua
@@ -6,6 +6,26 @@ return {
   config = function()
     require("window-picker").setup({
       hint = "floating-big-letter",
+
+      -- Whether to show 'Pick window:' prompt
+      show_prompt = false,
+
+      filter_rules = {
+        include_current_win = false,
+
+        -- When there is only one window available to pick from, DO NOT use
+        -- that window without prompting the user to select
+        autoselect_one = false,
+
+        -- filter using buffer options
+        bo = {
+          -- if the file type is one of following, the window will be ignored
+          filetype = { "neo-tree", "neo-tree-popup", "notify" },
+
+          -- if the buffer type is one of following, the window will be ignored
+          buftype = { "terminal", "quickfix" },
+        },
+      },
     })
   end,
 }
