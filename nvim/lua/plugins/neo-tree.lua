d7bd1aaebd59cf8aa2ad084322ed8f01fda6989e
Oğulcan Girginç <ogulcan@girginc.com>
2024-11-15 13:41:30 +0300

    Add relative line numbers to neo-tree

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.number = true
+          vim.opt_local.relativenumber = true
+        end,
+      },
+    },
+  },
+}
