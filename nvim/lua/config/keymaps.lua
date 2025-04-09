073b98606b31ea553492f9e16ab4b4b9d4894554
Oğulcan Girginç <ogirginc@ogirginc-M4.local>
2025-04-09 16:17:24 +0300

    Add buffer rel path yank

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- BUFFERS
+vim.keymap.set("n", "<leader>by", function()
+  vim.fn.setreg(vim.v.register, vim.fn.expand("%:."))
+end, { desc = "Yank buffer's path to clipboard" })
