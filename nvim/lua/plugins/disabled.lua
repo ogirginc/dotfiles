941d053f1bc4c8ccc533febdf394320f29ccd860
Oğulcan Girginç <ogulcan@girginc.com>
2026-03-30 20:38:51 +0300

    Disable copilot plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,7 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
+  { "zbirenbaum/copilot.lua", enabled = false },
+  { "zbirenbaum/copilot-cmp", enabled = false },
   -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
   -- { "echasnovski/mini.indentscope", enabled = false },
   -- { "nvim-treesitter/nvim-treesitter-context", enabled = false },
