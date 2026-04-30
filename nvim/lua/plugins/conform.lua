8445d06c9cb2405cdb718b54a9630b831f44b23d
Oğulcan Girginç <ogulcan@girginc.com>
2026-05-01 00:26:27 +0300

    Update conform lua

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,14 +1,37 @@
+local function erb_format_buffer()
+  require("conform").format({
+    formatters = { "erb_format" },
+    async = true,
+    lsp_format = "never",
+  })
+end
+
 return {
   {
     "stevearc/conform.nvim",
+    init = function()
+      vim.api.nvim_create_user_command("ErbFormat", erb_format_buffer, {
+        desc = "Format current buffer with erb-format (manual; bypasses save formatter)",
+      })
+    end,
+    keys = {
+      {
+        "<leader>cE",
+        erb_format_buffer,
+        mode = { "n", "v" },
+        desc = "Format with erb-format",
+        ft = "eruby",
+      },
+    },
     opts = {
       formatters_by_ft = {
         ruby = { "rubocop" },
         yaml = { "yamlfmt" },
+        eruby = { "herb_format" },
       },
       formatters = {
         rubocop = {
-          command = "/Users/ogirginc/.local/share/mise/shims/bundle",
+          command = "bundle",
           args = {
             "exec",
             "rubocop",
@@ -20,6 +43,13 @@ return {
             "--stdin",
             "$FILENAME",
           },
+          cwd = require("conform.util").root_file({ "Gemfile" }),
+          require_cwd = true,
+        },
+        herb_format = {
+          command = "bundle",
+          args = { "exec", "herb", "format", "-" },
+          stdin = true,
           cwd = require("conform.util").root_file({ "Gemfile" }),
           require_cwd = true,
         },
